a304f7860fd9cd495a99d75363c01b25ab4b5ec2
laojianzi <laojianzi1994@gmail.com>
2024-08-10 20:25:32 +0800

    chore: fix package doc link

--- parser/doc.go
+++ parser/doc.go
@@ -5,6 +5,6 @@ The goal of this project is to build a KQL parser in Go with the following key f
 
   - output AST to KQL query
 
-This project is inspired by [clickhouse-sql-parser](https://github.com/AfterShip/clickhouse-sql-parser) which is a SQL parser for ClickHouse in Go.
+This project is inspired by [github.com/AfterShip/clickhouse-sql-parser] which is a SQL parser for ClickHouse in Go.
 */
 package parser
